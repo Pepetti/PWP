ecba92cf3bf8243845c005106384eeabd9d2cc16
Oskari Kotajärvi <35844043+oskarikotajarvi@users.noreply.github.com>
2019-08-09 11:15:42 +0300

    Update day.js

--- backend/models/day.js
+++ backend/models/day.js
@@ -9,7 +9,7 @@ const ActivitySchema = require('./activity.js');
 
 //The day schema
 const DaySchema = new Schema({
-    //Date in format "23062019" (ddmmyyyy). Days are unique
+    //Date in format "23062019" (ddmmyyyy).
     date: {type: String, },
     activities: [ActivitySchema], //Day contains activities
 });
